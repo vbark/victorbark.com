89758b448ca8cd9b3ccb444bd568c4551f877c8a
vbark <victor.bark96@gmail.com>
2023-01-03 09:05:21 +0100

    added .cpanel.yml file and style.css with link.

--- index.php
+++ index.php
@@ -3,30 +3,13 @@
 <head>
   <title>My Personal Webpage</title>
   <link rel="icon" type="image/png" href="letter-v.png">
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-      background-color: #fafafa;
-      text-align: center;
-    }
-    h1 {
-      color: #333;
-      margin: 20px 0;
-    }
-    p {
-      margin: 20px 0;
-    }
-    a {
-      color: #1e90ff;
-      text-decoration: none;
-    }
-  </style>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <h1>Hello!</h1>
   <p>My name is Victor Bark and I am a IT-Engineer.</p>
   <p>Feel free to <a href="mailto:[me@victorbark.com]">contact me</a>.</p>
-  <a href="https://www.flaticon.com/free-icons/letter-v" title="letter v icons">Letter v icons created by icon_small - Flaticon</a>
+  <a href="https://www.flaticon.com/free-icons/letter-v" title="letter v icons" class="links">Letter v icons created by icon_small - Flaticon</a>
   <script>
     console.log('This is some JavaScript');
   </script>
