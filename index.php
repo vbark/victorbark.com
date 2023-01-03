f63b5842d3e68f528b85ab45dcb3d20eb9211d5c
vbark <victor.bark96@gmail.com>
2023-01-03 08:33:44 +0100

    favicon added

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
   <title>My Personal Webpage</title>
+  <link rel="icon" type="image/png" href="letter-v.png">
   <style>
     body {
       font-family: Arial, sans-serif;
@@ -25,6 +26,7 @@
   <h1>Hello!</h1>
   <p>My name is Victor Bark and I am a IT-Engineer.</p>
   <p>Feel free to <a href="mailto:[me@victorbark.com]">contact me</a>.</p>
+  <a href="https://www.flaticon.com/free-icons/letter-v" title="letter v icons">Letter v icons created by icon_small - Flaticon</a>
   <script>
     console.log('This is some JavaScript');
   </script>
